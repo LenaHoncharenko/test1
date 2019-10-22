3515bab0ceec8d2be426cfd640e8184afdfd0558
LenaHoncharenko <55978355+LenaHoncharenko@users.noreply.github.com>
2019-10-22 10:25:14 +0300

    Update test6.js

--- all/test6.js
+++ all/test6.js
@@ -8,8 +8,8 @@ let driver = new webdriver.Builder( )
     .forBrowser( 'chrome' )
     .build( );
 
-const userEmail = 'olenahoncharenko1994@gmail.com';
-const userPassword = '455515558a'
+const userEmail = 'ENTER EMAIL';
+const userPassword = 'ENTER PASSWORD'
 
 driver.get( 'https://my.novaposhta.ua/auth/index');
 

7fe563a84f333dfe7de98bd529b1da27b27caac8
LenaHoncharenko <55978355+LenaHoncharenko@users.noreply.github.com>
2019-10-22 15:26:37 +0300

    Update getUserCredentials.js

--- test/getUserCredentials.js
+++ test/getUserCredentials.js
@@ -1,7 +1,7 @@
 function getUserCredentials() {
     return {
-        email: "olenahoncharenko1994@gmail.com",
-        password: "455515558a"
+        email: "ENTER EMAIL",
+        password: "ENTER PASSWORD"
     }
 }
 

50d110017d4f7e414c0f604f375f399f7f5e24ed
LenaHoncharenko <55978355+LenaHoncharenko@users.noreply.github.com>
2019-10-22 17:24:25 +0300

    Update basePage.js

--- test/basePage.js
+++ test/basePage.js
@@ -1,11 +1,34 @@
-const { Builder, By, until } = require('selenium-webdriver');
-const chrome = require('selenium-webdriver/chrome');
-class driver {
-    constructor() {
-        this.driver = new Builder()
-            .forBrowser('chrome')
-            .build();
-    }
+require('chromedriver');
+
+let webdriver = require('selenium-webdriver'),
+    By = webdriver.By,
+    until = webdriver.until;
+
+let driver = function() {
+    return new webdriver.Builder()
+        .forBrowser('chrome')
+        .build();
 }
 
-module.exports = driver;
+async function waitAndClick(locator) {
+    await driver.wait(until.elementLocated(locator), 15000);
+    driver.findElement(locator).click();
+    return await driver.findElement(locator);
+};
+
+async function waitAndSendKeys(locator, value) {
+    return await driver.wait(until.elementLocated(locator), 20000)
+        .then(element => element.sendKeys(value));
+}
+
+async function waitElementIsVisible(locator) {
+    return await driver.wait(until.elementIsVisible(driver.findElement(locator)), 30000);
+}
+
+module.exports = {
+    'driver': driver,
+    'webdriver': webdriver,
+    'waitAndClick': waitAndClick,
+    'waitAndSendKeys': waitAndSendKeys,
+    'waitElementIsVisible': waitElementIsVisible,
+}
